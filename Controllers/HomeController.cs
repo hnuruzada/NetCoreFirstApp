e57c7fd0d42a5fa63878baa97d9662193b0b5aff
hnuruzada <90691865+hnuruzada@users.noreply.github.com>
2021-12-16 14:03:34 +0400

    First app

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            //ViewResult view = new ViewResult();
+            //view.ViewName = "Index";
+            //return view;
+            //return Content("Many Problems");
+            return View();
+        }
+
+        //about abouts olaraq deyisdirdim error vermedi cunku cshtml file adinda abouts.cshtml olaraq deyisdirdim
+        public ActionResult Abouts()
+        {
+            return View();
+        }
+        
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+    }
+}
